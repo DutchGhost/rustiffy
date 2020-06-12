bd08edec960b8eb9684f54d968d980eeffb9b179
Dodo <kasper199914@gmail.com>
2020-06-12 16:30:43 +0200

    more cases

--- src/main.zig
+++ src/main.zig
@@ -3,5 +3,9 @@ const std = @import("std");
 pub extern fn is_whitespace(c: u8) bool;
 
 pub fn main() anyerror!void {
-    std.debug.warn("All your codebase are belong to us. {}\n", .{is_whitespace(' ')});
+    const chars = [_]u8{ 'a', ' ', 'A', 0x09, 0x0A, 0x0D };
+
+    for (chars) |char, idx| {
+        std.debug.warn("{}: is '{c}' whitespace?: {}\n", .{ idx, char, is_whitespace(char) });
+    }
 }
